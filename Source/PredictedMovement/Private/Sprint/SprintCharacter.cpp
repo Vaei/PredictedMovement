61677aa194c9171b8a20e55c744861ce14194916
Vaei <jaredtaylor.99@gmail.com>
2023-02-11 10:15:33 +1300

    Added sprint input angle checking

--- Source/PredictedMovement/Private/Sprint/SprintCharacter.cpp
+++ Source/PredictedMovement/Private/Sprint/SprintCharacter.cpp
@@ -58,7 +58,30 @@ void ASprintCharacter::UnSprint(bool bClientSimulation)
 
 bool ASprintCharacter::CanSprint() const
 {
-	return !bIsSprinting && GetRootComponent() && !GetRootComponent()->IsSimulatingPhysics();
+	if (bIsSprinting || !GetRootComponent() && GetRootComponent()->IsSimulatingPhysics())
+	{
+		return false;
+	}
+		
+	// This check ensures that we are not sprinting backward or sideways, while allowing leeway 
+	// This angle allows sprinting when holding forward, forward left, forward right
+	// but not left or right or backward)
+	static constexpr float MaxSprintInputDegrees = 50.f;
+	static constexpr float MaxSprintInputNormal = 0.64278732;  // cos(rad(MaxSprintInputDegrees))
+
+	if constexpr (MaxSprintInputDegrees > 0.f)
+	{
+		if (SprintMovement)
+		{
+			const float Dot = (SprintMovement->GetCurrentAcceleration().GetSafeNormal2D() | GetActorForwardVector());
+			if (Dot < MaxSprintInputNormal)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
 void ASprintCharacter::OnEndSprint()

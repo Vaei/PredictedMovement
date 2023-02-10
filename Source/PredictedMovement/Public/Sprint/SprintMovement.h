0237722bfedabe06947eac1f38cf342338b2bd8d
Vaei <jaredtaylor.99@gmail.com>
2023-02-11 11:04:18 +1300

    Revert "Add braking frictions"
    This reverts commit d22ff4ad0f4f5065dace5daf6f3f01b9c31316ab.

--- Source/PredictedMovement/Public/Sprint/SprintMovement.h
+++ Source/PredictedMovement/Public/Sprint/SprintMovement.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
 
 #pragma once
 
@@ -42,17 +42,6 @@ public:
 	 */
 	UPROPERTY(Category="Character Movement: Walking", EditAnywhere, BlueprintReadWrite, meta=(ClampMin="0", UIMin="0"))
 	float GroundFrictionSprinting;
-
-	/**
-	 * Friction (drag) coefficient applied when braking (whenever Acceleration = 0, or if character is exceeding max speed); actual value used is this multiplied by BrakingFrictionFactor.
-	 * When braking, this property allows you to control how much friction is applied when moving across the ground, applying an opposing force that scales with current velocity.
-	 * Braking is composed of friction (velocity-dependent drag) and constant deceleration.
-	 * This is the current value, used in all movement modes; if this is not desired, override it or bUseSeparateBrakingFriction when movement mode changes.
-	 * @note Only used if bUseSeparateBrakingFriction setting is true, otherwise current friction such as GroundFriction is used.
-	 * @see bUseSeparateBrakingFriction, BrakingFrictionFactor, GroundFriction, BrakingDecelerationWalking
-	 */
-	UPROPERTY(Category="Character Movement (General Settings)", EditAnywhere, BlueprintReadWrite, meta=(ClampMin="0", UIMin="0", EditCondition="bUseSeparateBrakingFriction"))
-	float BrakingFrictionSprinting;
 	
 public:
 	/** If true, try to Sprint (or keep Sprinting) on next update. If false, try to stop Sprinting on next update. */
@@ -84,7 +73,7 @@ public:
 	{
 		if (IsSprinting() && IsMovingOnGround())
 		{
-			Friction = (bUseSeparateBrakingFriction ? BrakingFrictionSprinting : GroundFrictionSprinting);
+			Friction = (bUseSeparateBrakingFriction ? BrakingFriction : GroundFrictionSprinting);
 		}
 		Super::ApplyVelocityBraking(DeltaTime, Friction, BrakingDeceleration);
 	}

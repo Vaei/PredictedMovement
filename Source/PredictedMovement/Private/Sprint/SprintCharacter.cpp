d373de84f179ca2d86216aa9b8ebf836c15a7649
Vaei <jaredtaylor.99@gmail.com>
2023-02-11 01:13:53 +1300

    added sprint

---
+++ Source/PredictedMovement/Private/Sprint/SprintCharacter.cpp
@@ -0,0 +1,72 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+
+#include "Sprint/SprintCharacter.h"
+
+#include "Net/UnrealNetwork.h"
+#include "Sprint/SprintMovement.h"
+
+ASprintCharacter::ASprintCharacter(const FObjectInitializer& OI)
+	: Super(OI.SetDefaultSubobjectClass<USprintMovement>(CharacterMovementComponentName))
+{
+	SprintMovement = Cast<USprintMovement>(GetCharacterMovement());
+}
+
+void ASprintCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(ThisClass, bIsSprinting, COND_SimulatedOnly);
+}
+
+void ASprintCharacter::OnRep_IsSprinting()
+{
+	if (SprintMovement)
+	{
+		if (bIsSprinting)
+		{
+			SprintMovement->bWantsToSprint = true;
+			SprintMovement->Sprint(true);
+		}
+		else
+		{
+			SprintMovement->bWantsToSprint = false;
+			SprintMovement->UnSprint(true);
+		}
+		SprintMovement->bNetworkUpdateReceived = true;
+	}
+}
+
+void ASprintCharacter::Sprint(bool bClientSimulation)
+{
+	if (SprintMovement)
+	{
+		if (CanSprint())
+		{
+			SprintMovement->bWantsToSprint = true;
+		}
+	}
+}
+
+void ASprintCharacter::UnSprint(bool bClientSimulation)
+{
+	if (SprintMovement)
+	{
+		SprintMovement->bWantsToSprint = false;
+	}
+}
+
+bool ASprintCharacter::CanSprint() const
+{
+	return !bIsSprinting && GetRootComponent() && !GetRootComponent()->IsSimulatingPhysics();
+}
+
+void ASprintCharacter::OnEndSprint()
+{
+	K2_OnEndSprint();
+}
+
+void ASprintCharacter::OnStartSprint()
+{
+	K2_OnStartSprint();
+}
---
+++ Source/PredictedMovement/Public/Sprint/SprintCharacter.h
@@ -0,0 +1,83 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SprintCharacter.generated.h"
+
+class USprintMovement;
+UCLASS()
+class PREDICTEDMOVEMENT_API ASprintCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+	/** Movement component used for movement logic in various movement modes (walking, falling, etc), containing relevant settings and functions to control movement. */
+	UPROPERTY(Category=Character, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<USprintMovement> SprintMovement;
+
+	friend class FSavedMove_Character_Sprint;
+protected:
+	FORCEINLINE USprintMovement* GetSprintCharacterMovement() const { return SprintMovement; }
+	
+public:
+	/** Set by character movement to specify that this Character is currently Sprinting. */
+	UPROPERTY(BlueprintReadOnly, replicatedUsing=OnRep_IsSprinting, Category=Character)
+	uint32 bIsSprinting:1;
+	
+public:
+	// Sets default values for this character's properties
+	ASprintCharacter(const FObjectInitializer& OI);
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	/** Handle Crouching replicated from server */
+	UFUNCTION()
+	virtual void OnRep_IsSprinting();
+
+	/**
+	 * Request the character to start Sprinting. The request is processed on the next update of the CharacterMovementComponent.
+	 * @see OnStartSprint
+	 * @see IsSprinting
+	 * @see CharacterMovement->WantsToSprint
+	 */
+	UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+	virtual void Sprint(bool bClientSimulation = false);
+
+	/**
+	 * Request the character to stop Sprinting. The request is processed on the next update of the CharacterMovementComponent.
+	 * @see OnEndSprint
+	 * @see IsSprinting
+	 * @see CharacterMovement->WantsToSprint
+	 */
+	UFUNCTION(BlueprintCallable, Category=Character, meta=(HidePin="bClientSimulation"))
+	virtual void UnSprint(bool bClientSimulation = false);
+
+	/** @return true if this character is currently able to sprint (and is not currently sprinting) */
+	UFUNCTION(BlueprintCallable, Category=Character)
+	virtual bool CanSprint() const;
+	
+	/** 
+	 * Called when Character stops Sprinting. Called on non-owned Characters through bIsSprinting replication.
+	 */
+	virtual void OnEndSprint();
+
+	/** 
+	 * Event when Character stops Sprinting.
+	 */
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName="OnEndSprint", ScriptName="OnEndSprint"))
+	void K2_OnEndSprint();
+
+	/**
+	 * Called when Character Sprintes. Called on non-owned Characters through bIsSprinting replication.
+	 */
+	virtual void OnStartSprint();
+
+	/**
+	 * Event when Character Sprintes.
+	 */
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName="OnStartSprint", ScriptName="OnStartSprint"))
+	void K2_OnStartSprint();
+};

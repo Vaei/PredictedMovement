d6d3c461443736ebcf9aa13b0211d1f2e43def8d
Vaei <jaredtaylor.99@gmail.com>
2023-12-17 20:33:27 +1300

    Fix tooltip

--- Source/PredictedMovement/Public/Stamina/StaminaMovement.h
+++ Source/PredictedMovement/Public/Stamina/StaminaMovement.h
@@ -84,7 +84,7 @@ class PREDICTEDMOVEMENT_API UStaminaMovement : public UCharacterMovementComponen
 	GENERATED_BODY()
 
 public:
-	/** Maximum distance character is allowed to lag behind server location when interpolating between updates. */
+	/** Maximum stamina difference that is allowed between client and server before a correction occurs. */
 	UPROPERTY(Category="Character Movement (Networking)", EditDefaultsOnly, meta=(ClampMin="0.0", UIMin="0.0"))
 	float NetworkStaminaCorrectionThreshold;
 	

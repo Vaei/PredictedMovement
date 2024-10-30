a75a3a0f5e7d33fb9ece1da6709277ddc8351b5d
Jared Taylor <jaredtaylor.99@gmail.com>
2024-10-31 12:56:46 +1300

    Improve versioning

---
+++ Source/PredictedMovement/Public/System/PredictedMovementVersioning.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Misc/EngineVersionComparison.h"
+
+#ifndef UE_5_03_OR_LATER
+#if !UE_VERSION_OLDER_THAN(5, 3, 0)
+#define UE_5_03_OR_LATER 1
+#else
+#define UE_5_03_OR_LATER 0
+#endif
+#endif
+
+#ifndef UE_5_04_OR_LATER
+#if !UE_VERSION_OLDER_THAN(5, 4, 0)
+#define UE_5_04_OR_LATER 1
+#else
+#define UE_5_04_OR_LATER 0
+#endif
+#endif

12622217d0a34ddd9111fe2b2a2efdca7855518b
Jared Taylor <jaredtaylor.99@gmail.com>
2024-11-01 05:33:15 +1300

    Refactor debuff to modifier

---
+++ Source/PredictedMovement/Private/Modifier/ModifierCharacter.cpp
@@ -0,0 +1,101 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+#include "Modifier/ModifierCharacter.h"
+
+#include "Modifier/ModifierMovement.h"
+#include "Net/UnrealNetwork.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ModifierCharacter)
+
+AModifierCharacter::AModifierCharacter(const FObjectInitializer& FObjectInitializer)
+	: Super(FObjectInitializer.SetDefaultSubobjectClass<UModifierMovement>(CharacterMovementComponentName))
+{
+	ModifierMovement = Cast<UModifierMovement>(GetCharacterMovement());
+}
+
+void AModifierCharacter::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(AModifierCharacter, SimulatedSnare, COND_SimulatedOnly);
+}
+
+void AModifierCharacter::OnModifierChanged(const FGameplayTag& ModifierType, uint8 ModifierLevel, uint8 PrevModifierLevel)
+{
+	// Events for when a modifier is added or removed
+	if (ModifierLevel > 0 && PrevModifierLevel == 0)
+	{
+		OnModifierAdded(ModifierType, ModifierLevel);
+	}
+	else if (ModifierLevel == 0 && PrevModifierLevel > 0)
+	{
+		OnModifierRemoved(ModifierType, PrevModifierLevel);
+	}
+
+	// Replicate to simulated proxies
+	if (HasAuthority())
+	{
+		 SimulatedSnare = ModifierLevel;
+	}
+}
+
+void AModifierCharacter::OnModifierAdded(const FGameplayTag& ModifierType, uint8 ModifierLevel)
+{
+	// @TIP: Add Loose Gameplay Tag Here (Not Replicated)
+
+	K2_OnModifierAdded(ModifierType, ModifierLevel, 0);
+}
+
+void AModifierCharacter::OnModifierRemoved(const FGameplayTag& ModifierType, uint8 ModifierLevel)
+{
+	// @TIP: Remove Loose Gameplay Tag Here (Not Replicated)
+
+	K2_OnModifierRemoved(ModifierType, ModifierLevel, 0);
+}
+
+void AModifierCharacter::OnRep_SimulatedSnare(uint8 PrevSimulatedSnare)
+{
+	if (ModifierMovement)
+	{
+		ModifierMovement->Snare.ModifierLevel = SimulatedSnare;
+		ModifierMovement->bNetworkUpdateReceived = true;
+		
+		OnModifierChanged(FModifierTags::Modifier_Type_Debuff_Snare, SimulatedSnare, PrevSimulatedSnare);
+	}
+}
+
+void AModifierCharacter::Snare(ESnare SnareLevel)
+{
+	if (ModifierMovement)
+	{
+		ModifierMovement->Snare.AddModifier(static_cast<uint8>(SnareLevel));
+	}
+}
+
+void AModifierCharacter::RemoveSnare(ESnare SnareLevel)
+{
+	if (ModifierMovement)
+	{
+		ModifierMovement->Snare.RemoveModifier(static_cast<uint8>(SnareLevel));
+	}
+}
+
+bool AModifierCharacter::IsSnared() const
+{
+	return ModifierMovement && ModifierMovement->Snare.HasModifier();
+}
+
+ESnare AModifierCharacter::GetSnareLevel() const
+{
+	return ModifierMovement ? ModifierMovement->Snare.GetModifierLevel<ESnare>() : ESnare::None;
+}
+
+int32 AModifierCharacter::GetNumSnares() const
+{
+	return ModifierMovement ? ModifierMovement->Snare.GetNumModifiers() : 0;
+}
+
+int32 AModifierCharacter::GetNumSnaresByLevel(ESnare SnareLevel) const
+{
+	return ModifierMovement ? ModifierMovement->Snare.GetNumModifiersByLevel(static_cast<uint8>(SnareLevel)) : 0;
+}
---
+++ Source/PredictedMovement/Public/Modifier/ModifierCharacter.h
@@ -0,0 +1,70 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ModifierCharacter.generated.h"
+
+enum class ESnare : uint8;
+struct FGameplayTag;
+class UModifierMovement;
+
+UCLASS()
+class PREDICTEDMOVEMENT_API AModifierCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	/** Movement component used for movement logic in various movement modes (walking, falling, etc), containing relevant settings and functions to control movement. */
+	UPROPERTY(Category=Character, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UModifierMovement> ModifierMovement;
+	
+protected:
+	FORCEINLINE UModifierMovement* GetModifierCharacterMovement() const { return ModifierMovement; }
+
+public:
+	AModifierCharacter(const FObjectInitializer& FObjectInitializer);
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	virtual void OnModifierChanged(const FGameplayTag& ModifierType, uint8 ModifierLevel, uint8 PrevModifierLevel);
+	virtual void OnModifierAdded(const FGameplayTag& ModifierType, uint8 ModifierLevel);
+	virtual void OnModifierRemoved(const FGameplayTag& ModifierType, uint8 ModifierLevel);
+
+	UFUNCTION(BlueprintImplementableEvent, Category=Character, meta=(DisplayName="On Modifier Added"))
+	void K2_OnModifierAdded(const FGameplayTag& ModifierType, uint8 ModifierLevel, uint8 PrevModifierLevel);
+
+	UFUNCTION(BlueprintImplementableEvent, Category=Character, meta=(DisplayName="On Modifier Removed"))
+	void K2_OnModifierRemoved(const FGameplayTag& ModifierType, uint8 ModifierLevel, uint8 PrevModifierLevel);
+
+protected:
+	/* Snare (Non-Generic) Implementation -- Implement per-modifier type */
+	
+	UPROPERTY(ReplicatedUsing=OnRep_SimulatedSnare)
+	uint8 SimulatedSnare;
+
+	UFUNCTION()
+	void OnRep_SimulatedSnare(uint8 PrevSimulatedSnare);
+
+	UFUNCTION(BlueprintCallable, Category=Character)
+	void Snare(ESnare SnareLevel);
+
+	UFUNCTION(BlueprintCallable, Category=Character)
+	void RemoveSnare(ESnare SnareLevel);
+
+	UFUNCTION(BlueprintPure, Category=Character)
+	bool IsSnared() const;
+
+	UFUNCTION(BlueprintCallable, Category=Character)
+	ESnare GetSnareLevel() const;
+
+	UFUNCTION(BlueprintCallable, Category=Character)
+	int32 GetNumSnares() const;
+
+	UFUNCTION(BlueprintCallable, Category=Character)
+	int32 GetNumSnaresByLevel(ESnare SnareLevel) const;
+	
+	/* ~Snare (Non-Generic) Implementation */
+};

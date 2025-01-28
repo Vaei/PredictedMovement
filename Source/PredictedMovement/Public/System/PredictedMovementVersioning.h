859a5d488027b91be47a13954a607c0601eb0635
Jared Taylor <jaredtaylor.99@gmail.com>
2025-01-28 22:26:38 +1300

    Update versioning

--- Source/PredictedMovement/Public/System/PredictedMovementVersioning.h
+++ Source/PredictedMovement/Public/System/PredictedMovementVersioning.h
@@ -21,3 +21,11 @@
 #define UE_5_04_OR_LATER 0
 #endif
 #endif
+
+#ifndef UE_5_05_OR_LATER
+#if !UE_VERSION_OLDER_THAN(5, 5, 0)
+#define UE_5_05_OR_LATER 1
+#else
+#define UE_5_05_OR_LATER 0
+#endif
+#endif

abe611c78fa6cd9c15168d46f1b3773d9fdc2db6
Jared Taylor <jaredtaylor.99@gmail.com>
2025-02-05 07:43:53 +1300

    Merge Stamina to Pred (Sprint)

--- Source/PredictedMovement/Private/PredMovement.cpp
+++ Source/PredictedMovement/Private/PredMovement.cpp
@@ -7,6 +7,35 @@
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(PredMovement)
 
+void FPredMoveResponseDataContainer::ServerFillResponseData(const UCharacterMovementComponent& CharacterMovement,
+	const FClientAdjustment& PendingAdjustment)
+{
+	Super::ServerFillResponseData(CharacterMovement, PendingAdjustment);
+
+	// Server ➜ Client
+	const UPredMovement* MoveComp = Cast<UPredMovement>(&CharacterMovement);
+	bStaminaDrained = MoveComp->IsStaminaDrained();
+	Stamina = MoveComp->GetStamina();
+}
+
+bool FPredMoveResponseDataContainer::Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar,
+	UPackageMap* PackageMap)
+{
+	if (!Super::Serialize(CharacterMovement, Ar, PackageMap))
+	{
+		return false;
+	}
+
+	// Server ➜ Client
+	if (IsCorrection())
+	{
+		Ar << Stamina;
+		Ar << bStaminaDrained;
+	}
+
+	return !Ar.IsError();
+}
+
 void FPredNetworkMoveData::ClientFillNetworkMoveData(const FSavedMove_Character& ClientMove,
 	ENetworkMoveType MoveType)
 {
@@ -22,6 +51,7 @@ void FPredNetworkMoveData::ClientFillNetworkMoveData(const FSavedMove_Character&
 	
 	const FSavedMove_Character_Pred& SavedMove = static_cast<const FSavedMove_Character_Pred&>(ClientMove);
 	bWantsToSprint = SavedMove.bWantsToSprint;
+	Stamina = SavedMove.EndStamina;
 }
 
 bool FPredNetworkMoveData::Serialize(UCharacterMovementComponent& CharacterMovement, FArchive& Ar,
@@ -30,6 +60,7 @@ bool FPredNetworkMoveData::Serialize(UCharacterMovementComponent& CharacterMovem
 	Super::Serialize(CharacterMovement, Ar, PackageMap, MoveType);
 
 	Ar.SerializeBits(&bWantsToSprint, 1);
+	SerializeOptionalValue<float>(Ar.IsSaving(), Ar, Stamina, 0.f);
 	
 	return !Ar.IsError();
 }
@@ -38,6 +69,7 @@ UPredMovement::UPredMovement(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	SetNetworkMoveDataContainer(PredMoveDataContainer);
+	SetMoveResponseDataContainer(PredMoveResponseDataContainer);
 	
 	bUseMaxAccelerationSprintingOnlyAtSpeed = true;
 	MaxAccelerationSprinting = 1024.f;
@@ -49,6 +81,8 @@ UPredMovement::UPredMovement(const FObjectInitializer& ObjectInitializer)
 	VelocityCheckMitigatorSprinting = 0.98f;
 
 	bWantsToSprint = false;
+
+	NetworkStaminaCorrectionThreshold = 2.f;
 }
 
 bool UPredMovement::HasValidData() const
@@ -209,6 +243,81 @@ bool UPredMovement::IsSprintWithinAllowableInputAngle() const
 	return true;
 }
 
+
+void UPredMovement::SetStamina(float NewStamina)
+{
+	const float PrevStamina = Stamina;
+	Stamina = FMath::Clamp(NewStamina, 0.f, MaxStamina);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevStamina, Stamina))
+		{
+			OnStaminaChanged(PrevStamina, Stamina);
+		}
+	}
+}
+
+void UPredMovement::SetMaxStamina(float NewMaxStamina)
+{
+	const float PrevMaxStamina = MaxStamina;
+	MaxStamina = FMath::Max(0.f, NewMaxStamina);
+	if (CharacterOwner != nullptr)
+	{
+		if (!FMath::IsNearlyEqual(PrevMaxStamina, MaxStamina))
+		{
+			OnMaxStaminaChanged(PrevMaxStamina, MaxStamina);
+		}
+	}
+}
+
+void UPredMovement::SetStaminaDrained(bool bNewValue)
+{
+	const bool bWasStaminaDrained = bStaminaDrained;
+	bStaminaDrained = bNewValue;
+	if (CharacterOwner != nullptr)
+	{
+		if (bWasStaminaDrained != bStaminaDrained)
+		{
+			if (bStaminaDrained)
+			{
+				OnStaminaDrained();
+			}
+			else
+			{
+				OnStaminaDrainRecovered();
+			}
+		}
+	}
+}
+
+void UPredMovement::OnStaminaChanged(float PrevValue, float NewValue)
+{
+	if (FMath::IsNearlyZero(Stamina))
+	{
+		Stamina = 0.f;
+		if (!bStaminaDrained)
+		{
+			SetStaminaDrained(true);
+		}
+	}
+	// This will need to change if not using MaxStamina for recovery, here is an example (commented out) that uses
+	// 10% instead; to use this, comment out the existing else if statement, and change the 0.1f to the percentage
+	// you want to use (0.1f is 10%)
+	//
+	// else if (bStaminaDrained && Stamina >= MaxStamina * 0.1f)
+	// {
+	// 	SetStaminaDrained(false);
+	// }
+	else if (FMath::IsNearlyEqual(Stamina, MaxStamina))
+	{
+		Stamina = MaxStamina;
+		if (bStaminaDrained)
+		{
+			SetStaminaDrained(false);
+		}
+	}
+}
+
 void UPredMovement::UpdateCharacterStateBeforeMovement(float DeltaSeconds)
 {
 	// Proxies get replicated Sprint state.
@@ -268,11 +377,64 @@ bool UPredMovement::ClientUpdatePositionAfterServerUpdate()
 	return bResult;
 }
 
+void UPredMovement::OnClientCorrectionReceived(class FNetworkPredictionData_Client_Character& ClientData,
+	float TimeStamp, FVector NewLocation, FVector NewVelocity, UPrimitiveComponent* NewBase, FName NewBaseBoneName,
+	bool bHasBase, bool bBaseRelativePosition, uint8 ServerMovementMode
+#if UE_5_03_OR_LATER
+	, FVector ServerGravityDirection
+#endif
+	)
+{
+	// This occurs on AutonomousProxy, when the server sends the move response
+	// This is where we receive the snare, and can override the server's location, assuming it has given us authority
+
+	// Server >> SendClientAdjustment() ➜ ServerSendMoveResponse ➜ ServerFillResponseData() + MoveResponsePacked_ServerSend() >> Client
+	// >> ClientMoveResponsePacked() ➜ ClientHandleMoveResponse() ➜ ClientAdjustPosition_Implementation() ➜ OnClientCorrectionReceived
+	
+	const FPredMoveResponseDataContainer& PredMoveResponse = static_cast<const FPredMoveResponseDataContainer&>(GetMoveResponseDataContainer());
+
+	SetStamina(PredMoveResponse.Stamina);
+	SetStaminaDrained(PredMoveResponse.bStaminaDrained);
+	
+	Super::OnClientCorrectionReceived(ClientData, TimeStamp, NewLocation, NewVelocity, NewBase, NewBaseBoneName,
+		bHasBase, bBaseRelativePosition, ServerMovementMode, ServerGravityDirection);
+}
+
+bool UPredMovement::ServerCheckClientError(float ClientTimeStamp, float DeltaTime, const FVector& Accel,
+	const FVector& ClientWorldLocation, const FVector& RelativeClientLocation, UPrimitiveComponent* ClientMovementBase,
+	FName ClientBaseBoneName, uint8 ClientMovementMode)
+{
+	// ServerMovePacked_ServerReceive ➜ ServerMove_HandleMoveData ➜ ServerMove_PerformMovement
+	// ➜ ServerMoveHandleClientError ➜ ServerCheckClientError
+	
+	if (Super::ServerCheckClientError(ClientTimeStamp, DeltaTime, Accel, ClientWorldLocation, RelativeClientLocation, ClientMovementBase, ClientBaseBoneName, ClientMovementMode))
+	{
+		return true;
+	}
+    
+	/*
+	 * This will trigger a client correction if the Stamina value in the Client differs
+	 * NetworkStaminaCorrectionThreshold (2.f default) units from the one in the server
+	 * De-syncs can happen if we set the Stamina directly in Gameplay code (ie: GAS)
+	 */
+	const FPredNetworkMoveData* CurrentMoveData = static_cast<const FPredNetworkMoveData*>(GetCurrentNetworkMoveData());
+	if (!FMath::IsNearlyEqual(CurrentMoveData->Stamina, Stamina, NetworkStaminaCorrectionThreshold))
+	{
+		return true;
+	}
+    
+	return false;
+}
+
 void FSavedMove_Character_Pred::Clear()
 {
 	Super::Clear();
 
 	bWantsToSprint = false;
+	
+	bStaminaDrained = false;
+	StartStamina = 0.f;
+	EndStamina = 0.f;
 }
 
 void FSavedMove_Character_Pred::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel,
@@ -280,7 +442,65 @@ void FSavedMove_Character_Pred::SetMoveFor(ACharacter* C, float InDeltaTime, FVe
 {
 	Super::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
 
-	bWantsToSprint = Cast<APredCharacter>(C)->GetSprintCharacterMovement()->bWantsToSprint;
+	bWantsToSprint = Cast<APredCharacter>(C)->GetPredCharacterMovement()->bWantsToSprint;
+}
+
+bool FSavedMove_Character_Pred::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter,
+	float MaxDelta) const
+{
+	const TSharedPtr<FSavedMove_Character_Pred>& SavedMove = StaticCastSharedPtr<FSavedMove_Character_Pred>(NewMove);
+
+	if (bStaminaDrained != SavedMove->bStaminaDrained)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void FSavedMove_Character_Pred::CombineWith(const FSavedMove_Character* OldMove, ACharacter* C,
+	APlayerController* PC, const FVector& OldStartLocation)
+{
+	Super::CombineWith(OldMove, C, PC, OldStartLocation);
+
+	const FSavedMove_Character_Pred* SavedOldMove = static_cast<const FSavedMove_Character_Pred*>(OldMove);
+
+	if (UPredMovement* MoveComp = C ? Cast<UPredMovement>(C->GetCharacterMovement()) : nullptr)
+	{
+		MoveComp->SetStamina(SavedOldMove->StartStamina);
+		MoveComp->SetStaminaDrained(SavedOldMove->bStaminaDrained);
+	}
+}
+
+void FSavedMove_Character_Pred::SetInitialPosition(ACharacter* C)
+{
+	Super::SetInitialPosition(C);
+
+	if (const UPredMovement* MoveComp = C ? Cast<UPredMovement>(C->GetCharacterMovement()) : nullptr)
+	{
+		bStaminaDrained = MoveComp->IsStaminaDrained();
+		StartStamina = MoveComp->GetStamina();
+	}
+}
+
+void FSavedMove_Character_Pred::PostUpdate(ACharacter* C, EPostUpdateMode PostUpdateMode)
+{
+	// When considering whether to delay or combine moves, we need to compare the move at the start and the end
+	if (UPredMovement* MoveComp = C ? Cast<UPredMovement>(C->GetCharacterMovement()) : nullptr)
+	{
+		EndStamina = MoveComp->GetStamina();
+	
+		if (PostUpdateMode == PostUpdate_Record)
+		{
+			// Don't combine moves if the modifiers changed over the course of the move
+			if (bStaminaDrained != MoveComp->IsStaminaDrained())
+			{
+				bForceNoCombine = true;
+			}
+		}
+	}
+
+	Super::PostUpdate(C, PostUpdateMode);
 }
 
 FSavedMovePtr FNetworkPredictionData_Client_Character_Pred::AllocateNewMove()

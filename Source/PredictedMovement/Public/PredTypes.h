9eb8e5fc00b34057e82b15eed812120f18c84bab
Jared Taylor <jaredtaylor.99@gmail.com>
2025-02-15 11:52:58 +1300

    Full gait support incl walk/stroll

---
+++ Source/PredictedMovement/Public/PredTypes.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 Jared Taylor. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PredTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EPredGaitMode : uint8
+{
+	Stroll,
+	Walk,
+	Run,
+	Sprint,
+};

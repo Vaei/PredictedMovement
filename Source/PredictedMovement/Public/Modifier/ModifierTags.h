94ea02ad66306ae81874f284ab4f2a7f0501fbe7
Jared Taylor <jaredtaylor.99@gmail.com>
2025-06-29 10:47:52 +1200

    Add modifier types

---
+++ Source/PredictedMovement/Public/Modifier/ModifierTags.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Jared Taylor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace FModifierTags
+{
+	PREDICTEDMOVEMENT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Modifier_Boost);
+	PREDICTEDMOVEMENT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Modifier_SlowFall);
+	PREDICTEDMOVEMENT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Modifier_Snare);
+	
+	PREDICTEDMOVEMENT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(ClientAuth_Snare);
+}

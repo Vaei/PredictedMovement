4314ca4d8ecc8b13f0114bde97879f490155e8fe
Jared Taylor <jaredtaylor.99@gmail.com>
2025-06-30 11:20:02 +1200

    Add tags used for demo content

--- Source/PredictedMovement/Private/Modifier/ModifierTags.cpp
+++ Source/PredictedMovement/Private/Modifier/ModifierTags.cpp
@@ -12,5 +12,12 @@ namespace FModifierTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Modifier_SlowFall,	"Modifier.SlowFall", "Reduce Gravity to a % of its normal value");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Modifier_Snare,		"Modifier.Snare", "Reduced Movement Speed applied by server");
 
+	// You may want to fork and delete these, they are used for demo content
+	UE_DEFINE_GAMEPLAY_TAG(Modifier_Boost_Test_Level1, "Modifier.Boost.Test.Level1");
+	UE_DEFINE_GAMEPLAY_TAG(Modifier_Boost_Test_Level2, "Modifier.Boost.Test.Level2");
+	UE_DEFINE_GAMEPLAY_TAG(Modifier_Boost_Test_Level3, "Modifier.Boost.Test.Level3");
+	UE_DEFINE_GAMEPLAY_TAG(Modifier_Boost_Test_Level4, "Modifier.Boost.Test.Level4");
+	UE_DEFINE_GAMEPLAY_TAG(Modifier_Boost_Test_Level5, "Modifier.Boost.Test.Level5");
+	
 	UE_DEFINE_GAMEPLAY_TAG(ClientAuth_Snare,	"ClientAuth.Snare");
 }

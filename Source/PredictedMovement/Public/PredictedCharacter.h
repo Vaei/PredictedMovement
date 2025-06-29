0d70f9e5fe4d97ed67a09ed08837be082e0e3892
Jared Taylor <jaredtaylor.99@gmail.com>
2025-06-29 15:54:33 +1200

    Class descriptions

--- Source/PredictedMovement/Public/PredictedCharacter.h
+++ Source/PredictedMovement/Public/PredictedCharacter.h
@@ -15,7 +15,16 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FStaminaChangeEvent, float, Stamina
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStaminaEvent);
 
 /**
- * @TODO description
+ * A complete character class combined from the PredictedMovement repo.
+ *
+ * Separates movement property getters between base and scalar functions
+ * 
+ * Stackable modifiers: Boost, Haste, Slow, Snare, and SlowFall
+ *	Supports local predicted, local predicted with corrections, server-initiated
+ * Partial client authority for server-initiated movement
+ * 
+ * Gait modes: Stroll, Walk, Run, Sprint
+ * Features: Prone, Stamina, AimDownSights
  */
 UCLASS()
 class PREDICTEDMOVEMENT_API APredictedCharacter : public ACharacter
